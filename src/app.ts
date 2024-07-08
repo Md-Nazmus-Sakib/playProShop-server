b78471188b69be713f66f0443896b519d85b6c0e
Md-Nazmus-Sakib <nazmussakib0036@gmail.com>
2024-07-09 02:45:08 +0600

    global-error-handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import { ProductRoutes } from "./app/modules/product/product.route";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 
 const app: Application = express();
 
@@ -14,5 +15,7 @@ app.use("/api/product", ProductRoutes);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+// Global Error Handel
+app.use(globalErrorHandler);
 
 export default app;
---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,88 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ErrorRequestHandler } from "express";
+import { TErrorSources } from "../interface/error";
+import { ZodError } from "zod";
+import handleZodError from "../errors/handleZodError";
+import handleValidationError from "../errors/handleValidationError";
+import handleCastError from "../errors/handleCastError";
+import handleDuplicateError from "../errors/handleDuplicateError";
+import AppError from "../errors/AppError";
+import config from "../config";
+
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  // Default Value
+  let statusCode = 500;
+  let message = "Something Went Wrong !";
+
+  // default message
+  let errorSources: TErrorSources = [
+    {
+      path: "",
+      message: "Something Went Wrong",
+    },
+  ];
+  //Zod Error Handler
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
+  //Mongoose Error Handler
+  else if (err?.name === "ValidationError") {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
+  //Unknown route Cast Error Handler
+  else if (err?.name === "CastError") {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
+
+  // Already Exist Error
+  else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
+
+  //AppError Handler
+  else if (err instanceof AppError) {
+    statusCode = err?.statusCode;
+    message = err.message;
+    errorSources = [
+      {
+        path: "",
+        message: err?.message,
+      },
+    ];
+  }
+
+  //Ultimate error handler
+  else if (err instanceof Error) {
+    message = err.message;
+    errorSources = [
+      {
+        path: "",
+        message: err?.message,
+      },
+    ];
+  }
+
+  //Final Return is below
+
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    stack: config.NODE_ENV === "development" ? err?.stack : null,
+  });
+};
+
+export default globalErrorHandler;
