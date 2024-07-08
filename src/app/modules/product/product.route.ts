c0998802c8f1aaf993c31ed882c8e2be89f1bfeb
Md-Nazmus-Sakib <nazmussakib0036@gmail.com>
2024-07-09 03:20:15 +0600

    validation-handle

--- src/app/modules/product/product.route.ts
+++ src/app/modules/product/product.route.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { ProductControllers } from "./product.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { ProductValidation } from "./product.validation";
 
 const router = Router();
 
-router.post("/create-product", ProductControllers.createProduct);
+router.post(
+  "/create-product",
+  validateRequest(ProductValidation.productValidationSchema),
+  ProductControllers.createProduct
+);
 
 export const ProductRoutes = router;
